94b82d5c2ef1680bb7f955eb5951f8bd9bdfce64
Sam Gunner <me@sgunner.me>
2023-06-19 23:01:42 +0100

    WIP

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
+import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, RoomModule],
   controllers: [AppController],
   providers: [AppService],
 })
