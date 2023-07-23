d4ce9b0689a91612b7ddf79ee11d6a46f3b194e8
Sam Gunner <me@sgunner.me>
2023-07-23 14:31:42 +0100

    Docker fixes

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
 import { RoomModule } from './room/room.module';
 
 @Module({
-  imports: [DatabaseModule, RoomModule],
+  imports: [RoomModule],
   controllers: [AppController],
   providers: [AppService],
 })
